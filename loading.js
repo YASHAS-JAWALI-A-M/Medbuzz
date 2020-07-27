94558597ea7123fbd712e7724fa420f6ddc1dde9
YASHAS JAWALI A M <68346595+YASHAS-JAWALI-A-M@users.noreply.github.com>
2020-07-27 09:53:49 +0530

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,37 @@
+import React,{Component} from 'react';
+import {View,Text,StyleSheet,ActivityIndicator} from 'react-native';
+import * as firebase from 'firebase';
+
+class loading extends Component{
+    componentDidMount(){
+        this.unsuscribeAuth = firebase.auth().onAuthStateChanged((user)=>{
+            if(user){
+                this.props.navigation.navigate("login")
+            } 
+            else{
+                this.props.navigation.navigate("login")
+            }
+        })
+    }
+      
+    componentWillUnmount(){
+        this.unsuscribeAuth()
+    }
+    
+    render(){
+        return(
+            <View style={styles.act}>
+                <ActivityIndicator size="large" color="black" />
+            </View>
+        );
+    };
+} 
+const styles = StyleSheet.create({
+    act:{
+        flex:1,
+        justifyContent:'center',
+    fontSize:26
+    },
+})
+
+export default loading;
